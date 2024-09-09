395d0ef1521337c1945586c3c91de0975dcc5249
Kamui2134 <ctorkey@gmail.com>
2024-09-09 21:02:01 +0300

    Work with nginx

--- art-aksessoriz/nuxt.config.ts
+++ art-aksessoriz/nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import path from 'path'
+import fs from 'fs'
+
 export default defineNuxtConfig({
 	compatibilityDate: '2024-04-03',
 	css: ['~/assets/css/main.css'],
